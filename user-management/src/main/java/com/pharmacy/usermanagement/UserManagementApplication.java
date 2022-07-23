1fc4671dbdb901952a62eea38cc064229f05cd1e
vish-krishna <21krishna22@gmail.com>
2022-07-23 13:26:11 +0530

    user management update

--- user-management/src/main/java/com/pharmacy/usermanagement/UserManagementApplication.java
+++ user-management/src/main/java/com/pharmacy/usermanagement/UserManagementApplication.java
@@ -1,6 +1,5 @@
 package com.pharmacy.usermanagement;
 
-import org.hibernate.dialect.MySQL8Dialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,8 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class UserManagementApplication {
-
-	MySQL8Dialect dialect;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementApplication.class, args);
