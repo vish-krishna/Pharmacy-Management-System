2653a3270b17057e7ccc18705520245a5ab1cbc0
vish-krishna <21krishna22@gmail.com>
2022-08-22 20:17:56 +0530

    supplier bug fix

--- supplier-management/src/test/java/com/pharmacy/suppliermanagement/SupplierManagementApplicationTests.java
+++ supplier-management/src/test/java/com/pharmacy/suppliermanagement/SupplierManagementApplicationTests.java
@@ -22,7 +22,7 @@ class SupplierManagementApplicationTests {
 	@Test
 	public void getSupplierTest() {
 		when(supplierRepository.findAll()).thenReturn(Stream
-				.of(new Supplier(1,"name1","email1",123))
+				.of(new Supplier(1,"name1","email1","123"))
 				.collect(Collectors.toList()));
 		assertEquals(1,supplierRepository.findAll().size());
 	}
@@ -30,21 +30,21 @@ class SupplierManagementApplicationTests {
 	@Test
 	public void getSupplierByIdTest() {
 		long id=1;
-		Supplier supplier = new Supplier(1,"name1","email1",123);
+		Supplier supplier = new Supplier(1,"name1","email1","123");
 		when(supplierRepository.findSupplierBySupplierId(id)).thenReturn(supplier);
 		assertEquals(id,supplier.getSupplierId());
 	}
 
 	@Test
 	public void createSupplierTest() {
-		Supplier supplier = new Supplier(1,"name1","email1",1);
+		Supplier supplier = new Supplier(1,"name1","email1","123");
 		when(supplierRepository.save(supplier)).thenReturn(supplier);
 		assertEquals(supplier,supplierRepository.save(supplier));
 	}
 
 	@Test
 	public void deleteSupplierTest() {
-		Supplier supplier = new Supplier(1,"name1","email1",1);
+		Supplier supplier = new Supplier(1,"name1","email1","123");
 		supplierRepository.delete(supplier);
 		verify(supplierRepository,times(1)).delete(supplier);
 	}
